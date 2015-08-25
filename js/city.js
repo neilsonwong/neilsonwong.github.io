1b3a6580ba538dbc36ddf493138e49f6404528f4
Neilson <hc3wong@uwaterloo.ca>
2015-08-26 03:51:41 +0800

    added flight tracking and removed boston

---
+++ js/city.js
@@ -0,0 +1,141 @@
+function drawCity(city) {
+    var fixedDirection = (360 - city.orientation);
+    var angle = (fixedDirection / 360) * 2 * Math.PI;
+
+    displayCityName(city.name, city.distance, angle);
+    drawImage(city.distance / 50, angle);
+    var appropriateFlight = findFlight(city.airport);
+    if (appropriateFlight){
+        displayAirlineDetails(city.distance, angle, appropriateFlight.time, appropriateFlight.origin, appropriateFlight.destination, appropriateFlight.carrier, appropriateFlight.flight);
+    }
+}
+
+function drawImage(distance, angle) {
+
+    var xx = Math.round(distance * Math.sin(angle));
+    var zz = Math.round(distance * Math.cos(angle));
+    // var imgSize = 
+
+
+    var img = new THREE.MeshBasicMaterial({ //CHANGED to MeshBasicMaterial
+        map: THREE.ImageUtils.loadTexture('img/city.png'),
+        transparent: true,
+        color: 0x00FFFF
+    });
+
+    var plane = new THREE.Mesh(new THREE.PlaneGeometry(10, 10), img);
+    plane.position.z = zz;
+    plane.position.x = xx;
+    plane.position.y = 15;
+    plane.rotation.y = 0.5 * (2 * Math.PI) + angle;
+    scene.add(plane);
+}
+
+function displayCityName(name, distance, angle) {
+    //2PI radians per circle
+    var scaledDistance = distance / 25;
+
+    // var xx = Math.round(distance * Math.sin(angle));
+    // var zz = Math.round(distance * Math.cos(angle));
+
+    //set the text size to scale based on the distance
+    var textSize = 1 + Math.round(distance / 1455);
+    var textDetails = {
+        size: textSize,
+        height: 1
+    };
+
+    var cityText = new THREE.TextGeometry(name, textDetails);
+    var cityTextMesh = new THREE.Mesh(cityText, new THREE.MeshBasicMaterial({
+        color: 0xffffff,
+        opacity: 1
+    }));
+
+    //convert from polar to cartesian coordinates
+    cityTextMesh.position.z = scaledDistance * Math.cos(angle);
+    cityTextMesh.position.x = scaledDistance * Math.sin(angle);
+
+    //life the text up by 20
+    cityTextMesh.position.y = 20;
+
+    //rotate the text such that they face toward the origin at a readable angle
+    cityTextMesh.rotation.x = 0;
+    cityTextMesh.rotation.y = 0.5 * (2 * Math.PI) + angle;
+    scene.add(cityTextMesh);
+}
+
+function displayAirlineDetails(distance, angle, time, origin, destination, carrier, flightNumber){
+    var scaledDistance = distance / 25;
+
+    //set the text size to scale based on the distance
+    var textSize = 1 + Math.max(0, Math.round(distance / 1455)-1);
+    var textDetails = {
+        size: textSize,
+        height: 1
+    };
+    var meshDetails = {
+        color: 0xffffff,
+        opacity: 1
+    };
+
+    var lineOneText = new THREE.TextGeometry(time, textDetails);
+    var lineTwoText = new THREE.TextGeometry(carrier + flightNumber, textDetails);
+    var lineThreeText = new THREE.TextGeometry(origin + ' -> ' + destination, textDetails);
+
+    var lineOneTextMesh = new THREE.Mesh(lineOneText, new THREE.MeshBasicMaterial(meshDetails));
+    var lineTwoTextMesh = new THREE.Mesh(lineTwoText, new THREE.MeshBasicMaterial(meshDetails));
+    var lineThreeTextMesh = new THREE.Mesh(lineThreeText, new THREE.MeshBasicMaterial(meshDetails));
+
+    var positionZ = scaledDistance * Math.cos(angle);
+    var positionX = scaledDistance * Math.sin(angle);
+    var rotationY = 0.5 * (2 * Math.PI) + angle;
+
+    //convert from polar to cartesian coordinates
+    lineOneTextMesh.position.z = positionZ;
+    lineOneTextMesh.position.x = positionX;
+    lineTwoTextMesh.position.z = positionZ;
+    lineTwoTextMesh.position.x = positionX;
+    lineThreeTextMesh.position.z = positionZ;
+    lineThreeTextMesh.position.x = positionX;
+
+    var scaledHeight = textSize+23;
+    console.log(scaledHeight + destination);
+
+    //life the text up by 20
+    lineOneTextMesh.position.y = scaledHeight + 2*textSize*1.5;
+    lineTwoTextMesh.position.y = scaledHeight + textSize*1.5;
+    lineThreeTextMesh.position.y = scaledHeight;
+
+    //rotate the text such that they face toward the origin at a readable angle
+    lineOneTextMesh.rotation.x = 0;
+    lineOneTextMesh.rotation.y = rotationY;
+    lineTwoTextMesh.rotation.x = 0;
+    lineTwoTextMesh.rotation.y = rotationY;
+    lineThreeTextMesh.rotation.x = 0;
+    lineThreeTextMesh.rotation.y = rotationY;
+
+    //add the textMeshes to picture
+    scene.add(lineOneTextMesh);
+    scene.add(lineTwoTextMesh);
+    scene.add(lineThreeTextMesh);
+}
+
+function findFlight(destination){
+    var d = new Date();
+    var hour = d.getHours();
+    var min = d.getMinutes();
+
+    function matchDetails(val){
+        if (val.destination === destination){
+            var temp = val.time.split(':');
+            if ((temp[0] > hour) || (temp[0] == hour && temp[1] > min)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //we assume the matches are sorted in order of time
+    var matches = flightData.filter(matchDetails);
+    return (matches.length > 0) ? matches[0] : null;
+}
--- js/flights.js
+++ js/flights.js
@@ -6,67 +6,56 @@ var scene,
     effect,
     controls,
     clock,
+    flightData;
 
-    // Particles
-    particles = new THREE.Object3D(),
-    totalParticles = 200,
-    maxParticleSize = 200,
-    particleRotationSpeed = 0,
-    particleRotationDeg = 0,
-    lastColorRange = [0, 0.3],
-    currentColorRange = [0, 0.3],
+var cities = [
+  {name: 'Toronto', distance: 1599, orientation: 68, airport: 'YYZ'},
+  {name: 'London', distance: 6998, orientation: 79, airport: 'LHR'},
+  // {name: 'Boston', distance: 2010, orientation: 80, airport: 'BOS'},
+  {name: 'New York City', distance: 1762, orientation: 88, airport: 'JFK'},
+  {name: 'Birmingham', distance: 933, orientation: 138, airport: 'BHM'},
+  {name: 'Melbourne', distance: 14942, orientation: 235, airport: 'MEL'},
+  {name: 'El Dorado Hills', distance: 2289, orientation: 269, airport: 'SMF'},
+  {name: 'Vancouver', distance: 2524, orientation: 296, airport: 'YVR'}
+];
 
-    // City and weather API set up
-    cities = [
-      {name: 'Toronto', distance: 1599, orientation: 68},
-      {name: 'London', distance: 6998, orientation: 79},
-      {name: 'Boston', distance: 2010, orientation: 80},
-      {name: 'New York City', distance: 1762, orientation: 88},
-      {name: 'Birmingham', distance: 933, orientation: 138},
-      {name: 'Melbourne', distance: 14942, orientation: 235},
-      {name: 'El Dorado Hills', distance: 2289, orientation: 269},
-      {name: 'Vancouver', distance: 2524, orientation: 296}
-    ],
-    cityWeather = {},
-    cityTimes = [],
-    currentCity = 0;
-    // currentCityText = new THREE.TextGeometry(),
-    // currentCityTextMesh = new THREE.Mesh();
-
-init();
+$.getJSON('MCI.json', function(data) {
+    flightData = data;
+    init();
+});
 
 function init() {
   scene = new THREE.Scene();
-  camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.001, 2000);
-  camera.position.set(0, 25, 0);
-  scene.add(camera);
 
+  //add the webGL renderer into the DOM
   renderer = new THREE.WebGLRenderer({ antialias: true });
   element = renderer.domElement;
   container = document.getElementById('webglviewer');
   container.appendChild(element);
+
+  //setup the stereoscopic view
   effect = new THREE.StereoEffect(renderer);
 
+  setupCamera();
   setupControls();
   setupFloor();
   setupLighting();
-  // displayCityName('N', 30, 0);
-
-  console.log(cities);
+  setupSkybox();
 
   var city, index;
   for (index in cities){
     city = cities[index];
-    console.log(city.name)
-    displayCityName(city.name, city.distance, city.orientation);
-    drawImage(city.distance/50, city.orientation);
+    drawCity(city);
   }
-  
-  setupSkybox();
-  // drawImage();
 
   clock = new THREE.Clock();
   animate();
+}
+
+function setupCamera(){
+  camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.001, 2000);
+  camera.position.set(0, 25, 0);
+  scene.add(camera);
 }
 
 function setupControls(){
@@ -115,8 +104,8 @@ function setupSkybox() {
     var scCube = THREE.ImageUtils.loadTextureCube(sides);
     scCube.format = THREE.RGBFormat;
     // prepare skybox material (shader)
-    var skyShader = THREE.ShaderLib["cube"];
-    skyShader.uniforms["tCube"].value = scCube;
+    var skyShader = THREE.ShaderLib.cube;
+    skyShader.uniforms.tCube.value = scCube;
     var skyMaterial = new THREE.ShaderMaterial( {
       fragmentShader: skyShader.fragmentShader, vertexShader: skyShader.vertexShader,
       uniforms: skyShader.uniforms, depthWrite: false, side: THREE.BackSide
@@ -149,58 +138,6 @@ function setupFloor(){
   scene.add(floor);
 }
 
-function drawImage(distance, direction){
-  var fixedDirection = (360 - direction);
-  var angle = (fixedDirection / 360) * 2 * Math.PI;
-  var xx = Math.round(distance * Math.sin(angle));
-  var zz = Math.round(distance * Math.cos(angle));
-  // var imgSize = 
-
-
-  var img = new THREE.MeshBasicMaterial({ //CHANGED to MeshBasicMaterial
-        map:THREE.ImageUtils.loadTexture('img/city.png'),
-        transparent: true, 
-        color: 0x00FFFF
-        // wireframe: truedd
-  });
-  
-  var plane = new THREE.Mesh(new THREE.PlaneGeometry(10, 10), img);
-  plane.position.z = zz;
-  plane.position.x = xx;
-  plane.position.y = 15;
-  plane.rotation.y = 0.5 * (2 * Math.PI) + angle;
-  scene.add(plane);
-}
-
-function displayCityName(name, distance, direction) {
-  var scaledDistance = distance / 25;
-  var fixedDirection = (360 - direction);
-  var angle = (fixedDirection / 360) * 2 * Math.PI;
-  // var xx = Math.round(distance * Math.sin(angle));
-  // var zz = Math.round(distance * Math.cos(angle));
-  // var textSize = Math.max(10, Math.round(distance/1000));
-   var textSize = 1 + Math.round(distance/1455);
-   console.log(name +': '+ textSize);
-
-  var cityText = new THREE.TextGeometry(name, {
-    size: textSize,
-    height: 1
-  });
-  var cityTextMesh = new THREE.Mesh(cityText, new THREE.MeshBasicMaterial({
-    color: 0xffffff, opacity: 1
-  }));
-
-  cityTextMesh.position.z = scaledDistance * Math.cos(angle);
-  cityTextMesh.position.x = scaledDistance * Math.sin(angle);
-  cityTextMesh.position.y = 20;
-
-  cityTextMesh.rotation.x = 0;
-  //2PI radians per circle
-  // cityTextMesh.rotation.y = ;
-  cityTextMesh.rotation.y = 0.5 * (2 * Math.PI) + angle;
-  // cityTextMesh.rotation.y = -Math.PI/2;//  (direction / 360) * Math.PI;
-  scene.add(cityTextMesh);
-}
 
 function animate() {
   requestAnimationFrame(animate);
